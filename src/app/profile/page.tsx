1407bbea3033e6a39066f1e13c747fa421522979
Roland Mungure <mungureroland@gmail.com>
2024-03-20 11:55:05 +0200

    middleware done

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import axios from "axios";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 
 function page() {
   const router = useRouter();
+
+  const [data, setData] = useState("nothing");
   const logout = async () => {
     try {
       const response = await axios.get("api/users/logout");
@@ -16,12 +18,33 @@ function page() {
       toast.error(error.message);
     }
   };
+
+  const getUserDatails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
   return (
-    <div>
+    <div className="flex flex-col min-h-screen items-center justify-center space-y-4">
       <h1>Profile</h1>
       <h2>Profile page</h2>
+      <h3>
+        {data === "nothing" ? (
+          "Nothing"
+        ) : (
+          <Link
+            href={`/profile/${data}`}
+            className=" bg-emerald-400 rounded-md p-2"
+          >
+            {data}
+          </Link>
+        )}
+      </h3>
       <button className=" bg-emerald-500 px-5 py-2" onClick={logout}>
         Logout
+      </button>
+      <button className=" bg-emerald-800 px-5 py-2" onClick={getUserDatails}>
+        Get user Details
       </button>
     </div>
   );
--- src/helpers/getDataFromToken.ts
+++ src/helpers/getDataFromToken.ts
@@ -1,11 +1,15 @@
 
 import { NextRequest} from "next/server";
 import jwt from "jsonwebtoken";
+import { decode } from "punycode";
 
 export const getDataFromToken = (request: NextRequest) => { 
 
     try {
-        request.cookies.get('token')?.value || ``;
+
+        const token = request.cookies.get('token')?.value || ``;
+        const decodeToken:any = jwt.verify(token, process.env.TOKEN_SECRET!);
+        return decodeToken.id;
     } catch (error:any) {
         throw new Error(error.message);
     }
